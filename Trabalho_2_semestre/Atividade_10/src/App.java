885a08baa5d7a452829af0667223ae09fc04bee7
kirigaya7447 <joaoh2915@gmail.com>
2025-09-13 16:19:25 -0300

    Códigos atualizados

--- Trabalho_2_semestre/Atividade_10/src/App.java
+++ Trabalho_2_semestre/Atividade_10/src/App.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class app4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("escreva uma palavra: ");
+        String a = scan.nextLine();
+
+        for (int i = a.length() - 1; i >= 0; i--) {
+            System.out.println(a.charAt(i));
+        }
     }
 }
