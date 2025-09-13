3d93549989913b32f2a00b44781a1bf454f7c372
kirigaya7447 <joaoh2915@gmail.com>
2025-09-13 16:27:36 -0300

    Atividades de 1 a 5

--- Trabalho_2_semestre/Atividade_9/src/App.java
+++ Trabalho_2_semestre/Atividade_9/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite o ano: ");
+        int ano = scan.nextInt();
+        System.out.println("digite o mês: ");
+        int mes = scan.nextInt();
+        while (ano < 0) {
+            System.out.println("digite o ano: ");
+            ano = scan.nextInt();
+            System.out.println("digite o mês: ");
+            mes = scan.nextInt();
+        }
+        while (mes < 1 || mes > 12) {
+            System.out.println("digite o ano: ");
+            ano = scan.nextInt();
+            System.out.println("digite o mês: ");
+            mes = scan.nextInt();
+        }
     }
+
 }
