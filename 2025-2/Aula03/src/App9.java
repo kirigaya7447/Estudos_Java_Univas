9f4a7fe3d31b80dbdc0440bb9754e5d810a9af95
kirigaya7447 <joaoh2915@gmail.com>
2025-10-30 21:12:20 -0300

    Todas atividades realizadas

--- 2025-2/Aula03/src/App9.java
+++ 2025-2/Aula03/src/App9.java
@@ -1,10 +1,14 @@
 public class App9 {
     public static void main(String[] args){
-        /*Invertendo um Vetor (Usando for Clássico) Crie um vetor de int com os valores {10, 20, 30,
-40, 50}. O seu desafio é inverter a ordem dos elementos dentro do próprio vetor (o resultado
-deve ser {50, 40, 30, 20, 10}). Dica: Você não pode usar for each. Você precisará de um for
-clássico que vai só até a metade do vetor, trocando o elemento i com o elemento
-vetor.length - 1 - i. Isso reforça quando o for clássico é necessário. */
+        int nums[] = {10, 20, 30, 40, 50};
+        int numsContra[] = new int[nums.length];
+
+        for(int cont = 0; cont < nums.length; cont++){
+            numsContra[((nums.length - 1) - cont)] = nums[cont];
+        }
+
+        for(int cont = 0; cont < numsContra.length; cont++){
+            System.out.println(numsContra[cont]);
+        }
     }
-    
 }
