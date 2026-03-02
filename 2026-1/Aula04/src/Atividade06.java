dc4cb652724ff34032805c960ecfe513d7d5a503
kirigaya7447 <joaoh2915@gmail.com>
2026-03-02 08:09:55 -0300

    Continuando a tarefa

--- 2026-1/Aula04/src/Atividade06.java
+++ 2026-1/Aula04/src/Atividade06.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class Atividade06 {
     public static void main(String[] args) throws Exception {
     Scanner le = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        System.out.println(recebeIdade(le));
+    }
+
+    public static String recebeIdade(Scanner le){
+        System.out.println("Digite sua idade:")
+        int idade = le.nextInt();
+        if(isMaiorDeIdade(idade)){
+            return "É maior de idade!"
+        }
+        else{
+            return "Não é maior de idade!";
+        }
+    }
+
+    public static boolean isMaiorDeIdade(int idade){
+        boolean eMaior;
+        if(idade >= 18){
+            eMaior = true;
+        }
+        else if{
+            eMaior = false;
+        }
+
+        return eMaior;
     }
 }
--- 2026-1/Aula04/src/Atividade07.java
+++ 2026-1/Aula04/src/Atividade07.java
@@ -3,6 +3,37 @@ import java.util.Scanner;
 public class Atividade07 {
     public static void main(String[] args) throws Exception {
     Scanner le = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        double salario = recebeSalario(le);
+        double parcela = recebeParcela(le);
+        if(aprovarEmprestimo()){
+            System.out.println("Empréstimo aprovado!");
+        }
+        else{
+            System.out.println("Empréstimo negado!");
+        }
     }
+
+    public static double recebeSalario(Scanner le){
+        System.out.println("Digite o valor do seu salário:")
+        double salario = le.nextDouble();
+        return salario;
+    }
+
+    public static double recebeParcela(Scanner le){
+        System.out.println("Digite o valor da parcela a ser verificada:")
+        double parcela = le.nextDouble();
+        return parcela;
+    }
+
+    public static boolean aprovarEmprestimo(double salarioCliente, double parcelaEmprestimo){
+        boolean aprovado;
+        double porcentagem = (salario * 30) / 100;
+        if(parcelaEmprestimo > porcentagem){
+            aprovado = false;
+        }
+        else{
+            aprovado = true;
+        }
+    }
+    return aprovado;
 }
