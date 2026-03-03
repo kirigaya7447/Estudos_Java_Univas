329a752d2275852cda361b8760df0a6334bf907a
kirigaya7447 <joaoh2915@gmail.com>
2026-03-03 07:59:07 -0300

    Continuando aula04

--- 2026-1/Aula04/src/Atividade08.java
+++ 2026-1/Aula04/src/Atividade08.java
@@ -3,21 +3,36 @@ import java.util.Scanner;
 public class Atividade08 {
     public static void main(String[] args) throws Exception {
     Scanner le = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        System.out.println(recebeConsumo(le));
     }
 
-    public static void calcularContaAgua(double consumo){
-        if(consumo <= 10){
-            
-        }
-        else if(consumo > 10 && consumo <= 20){
-
-        }
-        else if(consumo > 20){
-
+    public static String recebeConsumo(Scanner le){
+        System.out.println("Digite a quantia de consumo de água no mês(em metros cúbicos):");
+        double consumo = le.nextDouble();
+        double consumoTotal = calcularContaAgua(consumo);
+        if(consumoTotal != 0){
+        return "A conta de água do mês será de R$" + consumoTotal;
         }
         else{
-            System.out.println("Informação divergente");
+        return "Insira um valor válido!";
         }
+    }
+
+    public static double calcularContaAgua(double consumo){
+        double consumoTotal;
+        if(consumo > 0 && consumo <= 10){
+            consumoTotal = 35 + consumo;
+        }
+        else if(consumo > 10 && consumo <= 20){
+            consumoTotal = consumo * 5;
+        }
+        else if(consumo > 20){
+            consumoTotal = consumo * 7;
+        }
+        else{
+            System.out.println("Informação inválida!");
+            consumoTotal = 0;
+        }
+        return consumoTotal;
     }
 }
--- 2026-1/Aula04/src/Atividade09.java
+++ 2026-1/Aula04/src/Atividade09.java
@@ -5,4 +5,12 @@ public class Atividade09 {
     Scanner le = new Scanner(System.in);
         System.out.println("Hello, World!");
     }
+
+    public static String formatarSegundos(int segundos){
+        int horas = segundos / 3600;
+        segundos = segundos % 3600;
+        int minutos = segundos * 60;
+        return horas + " horas, " + minutos + " minutos e" + segundos + " segundos";
+
+    }
 }
