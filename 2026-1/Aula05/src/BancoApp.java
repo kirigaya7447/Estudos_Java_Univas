103d521fb27de23bdfaa9a729a71145c5c203a9b
kirigaya7447 <joaoh2915@gmail.com>
2026-03-03 21:38:43 -0300

    Quase finalizando aula 05

--- 2026-1/Aula05/src/BancoApp.java
+++ 2026-1/Aula05/src/BancoApp.java
@@ -1,8 +1,60 @@
 import java.util.Scanner;
-import CaixaEletronico.sacar;
 
 public class BancoApp {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o valor do seu saldo: ");
+        double saldo = scan.nextInt();
+        int menu = 0;
+        while (menu != 9) {
+            menu = menu(scan, saldo);
+        }
+    }
+
+    private static int menu(Scanner scan, double saldo){
+        System.out.println("Selecione a opção desejada!" +
+        "\nOpção 1 - Sacar" + 
+        "\nOpção 2 - Depositar"+
+        "\nOpção 9 - Sair");
+        int menu = scan.nextInt();
+        if(menu == 1){
+            realizaSaque(scan, saldo);
+        }
+        else if(menu == 2){
+            realizaDeposito(scan, saldo);
+        }
+        else if(menu == 9){
+            menu = menu;
+        }
+        else{
+            System.out.println("Opção inválida!");
+            menu = 0;
+        }
+
+        return menu;
+    }
+
+    private static void realizaSaque(Scanner scan, double saldo) {
+        System.out.println("Digite o valor do saque: ");
+        int valorSaque = scan.nextInt();
+
+        double novoSaldo = CaixaEletronico.sacar(saldo, valorSaque);
         
+        if(saldo != novoSaldo){
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Novo valor do saldo: " + novoSaldo);
+        }
+    }
+
+    private static void realizaDeposito(Scanner scan, double saldo) {
+        System.out.println("Digite o valor do depósito: ");
+        int valorSaque = scan.nextInt();
+
+        double novoSaldo = CaixaEletronico.depositar(saldo, valorSaque);
+
+        if(saldo != novoSaldo){
+            System.out.println("Depósito realizado com sucesso!");
+            System.out.println("Novo valor do saldo: " + novoSaldo);
+        }
     }
 }
--- 2026-1/Aula05/src/CaixaEletronico.java
+++ 2026-1/Aula05/src/CaixaEletronico.java
@@ -1,6 +1,15 @@
 public class CaixaEletronico {
-    public double sacar(){
-        double saldo = 0;
+    public static double sacar(double saldo, double valorSaque){
+        if(saldo >= valorSaque){
+            saldo = saldo - valorSaque;
+        }
+        return saldo;
+    }
+
+    public static double depositar(double saldo, double valorDeposito){
+        if(valorDeposito > 0){
+            saldo = saldo + valorDeposito;
+        }
         return saldo;
     }
 }
