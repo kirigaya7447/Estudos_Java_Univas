289b9e94720ac8aa9349c31b72afa93479b2349a
kirigaya7447 <joaoh2915@gmail.com>
2026-03-12 20:06:27 -0300

    Fazendo trabalho01

--- 2026-1/Trabalho01/src/RH_Utilitarios.java
+++ 2026-1/Trabalho01/src/RH_Utilitarios.java
@@ -1,3 +1,59 @@
 public class RH_Utilitarios {
-    
+    public static double calcularINSS(double salarioBruto) {
+        double inssCalculado;
+        double desconto;
+        if (salarioBruto <= 2000) {
+            desconto = salarioBruto * 0.08;
+            inssCalculado = salarioBruto - desconto;
+        } else {
+            desconto = salarioBruto * 0.11;
+            inssCalculado = salarioBruto - desconto;
+        }
+
+        return inssCalculado;
+    }
+
+    public static double calcularImpostoRenda(double salarioBruto) {
+        double ir;
+        if (salarioBruto <= 2500) {
+            ir = 0;
+        } else if (salarioBruto > 2500.01 && salarioBruto <= 5000) {
+            ir = salarioBruto * 0.1;
+        } else {
+            ir = salarioBruto * 0.2;
+        }
+
+        return ir;
+    }
+
+    public static double calcularPlanoSaude(int idade) {
+        double desconto;
+        if (idade < 30) {
+            desconto = 100;
+        } else if (idade >= 30 && idade < 50) {
+            desconto = 200;
+        } else {
+            desconto = 300;
+        }
+
+        return desconto;
+    }
+
+    public static double calcularSalarioLiquido(double salarioBruto, double inss, double ir, double planoSaude) {
+        double salarioSubtraido;
+        salarioSubtraido = salarioBruto - inss - ir - planoSaude;
+        return salarioSubtraido;
+    }
+
+    public static String verificarCategoria(double salarioLiquido) {
+        String categoria;
+        if (salarioLiquido < 1500) {
+            categoria = "Estagiário";
+        } else if (salarioLiquido >= 1500 && salarioLiquido < 3000) {
+            categoria = "Júnior";
+        } else {
+            categoria = "Pleno/Sênior";
+        }
+        return categoria;
+    }
 }
--- 2026-1/Trabalho01/src/SistemaFolha.java
+++ 2026-1/Trabalho01/src/SistemaFolha.java
@@ -1,3 +1,12 @@
 public class SistemaFolha {
-    
+     public static void main(String[] args) throws Exception {
+        int cont = 0;
+        do{
+            System.out.println("=== SISTEMA DE FOLHA DE PAGAMENTO ===\n"+
+                                "1 - Processar lote de funcionários\n"+
+                                "2 - Sair do sistema\n"+
+                                "Escolha uma opção:");
+        }
+        while (cont != 2);
+     }
 }
